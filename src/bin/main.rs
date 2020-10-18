1fe1179692b0d6890ba597b3dfae07fa29a1324b
Andrzej Swatowski <andrzejswatowski@outlook.com>
2020-10-18 21:52:23 +0200

    Show validation layers

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,43 +1,66 @@
 use rust_game::utility;
-use rust_game::utility::constants;
+use rust_game::utility::{constants, debug, debug::ValidationInfo};
 
 use ash::version::{EntryV1_0, InstanceV1_0};
-use ash::vk;  // Vulkan API
+use ash::vk; // Vulkan API
 use std::ffi::CString;
+use std::os::raw::c_void;
 use std::ptr;
 
-use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
-use winit::event_loop::{EventLoop, ControlFlow};
+use winit::event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent};
+use winit::event_loop::{ControlFlow, EventLoop};
 use winit::window::Window;
 
 // Constants
-const WINDOW_TITLE: &'static str = "01.Instance Creation";
+const WINDOW_TITLE: &'static str = "02.Validation Layers";
+const VALIDATION: ValidationInfo = ValidationInfo {
+    is_enable: true,
+    required_validation_layers: ["VK_LAYER_KHRONOS_validation"],
+};
 
 struct VulkanApp {
     _entry: ash::Entry,
     instance: ash::Instance,
+    debug_utils_loader: ash::extensions::ext::DebugUtils,
+    debug_messenger: vk::DebugUtilsMessengerEXT,
 }
 
 impl VulkanApp {
     pub fn new() -> VulkanApp {
         let entry = ash::Entry::new().unwrap();
         let instance = VulkanApp::create_instance(&entry);
+        let (debug_utils_loader, debug_messenger) =
+            debug::setup_debug_utils(VALIDATION.is_enable, &entry, &instance);
 
         VulkanApp {
             _entry: entry,
             instance,
+            debug_utils_loader,
+            debug_messenger,
         }
     }
 
     fn init_window(event_loop: &EventLoop<()>) -> Window {
         winit::window::WindowBuilder::new()
             .with_title(WINDOW_TITLE)
-            .with_inner_size(winit::dpi::LogicalSize::new(constants::WINDOW_WIDTH, constants::WINDOW_HEIGHT))
+            .with_inner_size(winit::dpi::LogicalSize::new(
+                constants::WINDOW_WIDTH,
+                constants::WINDOW_HEIGHT,
+            ))
             .build(event_loop)
             .expect("Failed to create window.")
     }
 
     fn create_instance(entry: &ash::Entry) -> ash::Instance {
+        if VALIDATION.is_enable
+            && debug::check_validation_layer_support(
+                entry,
+                &VALIDATION.required_validation_layers.to_vec(),
+            ) == false
+        {
+            panic!("Validation layers requested, but not available!")
+        }
+
         let app_name = CString::new(WINDOW_TITLE).unwrap();
         let engine_name = CString::new("Vulkan Engine").unwrap();
 
@@ -51,15 +74,41 @@ impl VulkanApp {
             api_version: constants::API_VERSION,
         };
 
+        // This create info used to debug issues in vk::createInstance and vk::destroyInstance.
+        let debug_utils_create_info = debug::populate_debug_messenger_create_info();
+
         let extension_names = utility::platforms::required_extension_names();
+
+        let required_validation_layer_raw_names: Vec<CString> = VALIDATION
+            .required_validation_layers
+            .iter()
+            .map(|layer_name| CString::new(*layer_name).unwrap())
+            .collect();
+        let enable_layer_names: Vec<*const i8> = required_validation_layer_raw_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
 
         let create_info = vk::InstanceCreateInfo {
             s_type: vk::StructureType::INSTANCE_CREATE_INFO,
-            p_next: ptr::null(),
+            p_next: if VALIDATION.is_enable {
+                &debug_utils_create_info as *const vk::DebugUtilsMessengerCreateInfoEXT
+                    as *const c_void
+            } else {
+                ptr::null()
+            },
             flags: vk::InstanceCreateFlags::empty(),
             p_application_info: &app_info,
-            pp_enabled_layer_names: ptr::null(),
-            enabled_layer_count: 0,
+            pp_enabled_layer_names: if VALIDATION.is_enable {
+                enable_layer_names.as_ptr()
+            } else {
+                ptr::null()
+            },
+            enabled_layer_count: if VALIDATION.is_enable {
+                enable_layer_names.len()
+            } else {
+                0
+            } as u32,
             pp_enabled_extension_names: extension_names.as_ptr(),
             enabled_extension_count: extension_names.len() as u32,
         };
@@ -74,31 +123,25 @@ impl VulkanApp {
     }
 
     pub fn main_loop(self, event_loop: EventLoop<()>) {
-        event_loop.run(move |event, _, control_flow| {
-            match event {
-                | Event::WindowEvent { event, .. } => {
-                    match event {
-                        | WindowEvent::CloseRequested => {
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        virtual_keycode,
+                        state,
+                        ..
+                    } => match (virtual_keycode, state) {
+                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                            dbg!();
                             *control_flow = ControlFlow::Exit
-                        },
-                        | WindowEvent::KeyboardInput { input, .. } => {
-                            match input {
-                                | KeyboardInput { virtual_keycode, state, .. } => {
-                                    match (virtual_keycode, state) {
-                                        | (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
-                                            dbg!();
-                                            *control_flow = ControlFlow::Exit
-                                        },
-                                        | _ => {},
-                                    }
-                                },
-                            }
-                        },
-                        | _ => {},
-                    }
+                        }
+                        _ => {}
+                    },
                 },
-                _ => (),
-            }
+                _ => {}
+            },
+            _ => (),
         })
     }
 }
@@ -106,6 +149,10 @@ impl VulkanApp {
 impl Drop for VulkanApp {
     fn drop(&mut self) {
         unsafe {
+            if VALIDATION.is_enable {
+                self.debug_utils_loader
+                    .destroy_debug_utils_messenger(self.debug_messenger, None);
+            }
             self.instance.destroy_instance(None);
         }
     }
--- src/utility/mod.rs
+++ src/utility/mod.rs
@@ -1,2 +1,4 @@
 pub mod constants;
+pub mod debug;
 pub mod platforms;
+pub mod tools;
