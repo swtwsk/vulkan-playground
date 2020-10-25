63865c00d1ad24b4e418576aca22935f29ee10b5
Andrzej Swatowski <andrzejswatowski@outlook.com>
2020-10-25 16:45:47 +0100

    Do not call shader compilation on Windows

--- build.rs
+++ build.rs
@@ -5,7 +5,11 @@ fn main() {
     println!("cargo:rerun-if-changed=shaders/shader.frag");
     println!("cargo:rerun-if-changed=shaders/shader.vert");
 
-    Command::new("shaders/compile.sh")
-        .output()
-        .expect("Failed to run compile script");
+    if cfg!(windows) {
+        return;
+    } else {
+        Command::new("shaders/compile.sh")
+            .output()
+            .expect("Failed to run compile script");
+    }
 }
